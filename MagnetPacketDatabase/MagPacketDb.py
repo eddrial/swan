a2086f2b93b8aec22a9f3ef68943cfe095b0436b
Ed Rial <eddrial@hotmail.com>
2020-01-24 12:02:40 +0100

    Spin up procedural into an OOP

---
+++ MagnetPacketDatabase/MagPacketDb.py
@@ -0,0 +1,17 @@
+'''
+Created on 24 Jan 2020
+
+@author: oqb
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
---
+++ MagnetPacketDatabase/testdrive.py
@@ -0,0 +1,8 @@
+'''
+Created on 24 Jan 2020
+
+@author: oqb
+'''
+
+if __name__ == '__main__':
+    pass
